eba9ffaaaf500c07de893e36b4a485cfb16d89f6
Drew O'Meara <drew@plan-systems.org>
2023-07-06 00:20:03 -0500

    added PreventIdleClose()

--- stdlib/task/api.task.go
+++ stdlib/task/api.task.go
@@ -30,12 +30,13 @@ type Task struct {
 	// Note how this will not enter into effect unless OnRun is given or a child is started.
 	IdleClose time.Duration
 
-	TaskRef   any                     // Offered to you for open-ended use.
-	Label     string                  // Label is a log label and debugging
-	OnStart   func(ctx Context) error // Blocking fn called in StartChild(). If err, ctx.Close() is called and Go() returns the err and OnRun is never called.
-	OnRun     func(ctx Context)       // Async work body. If non-nil, ctx.Close() will be automatically called after OnRun() completes
-	OnClosing func()                  // Called after Close() is first called and immediately before children are signaled to close.
-	OnClosed  func()                  // Called after Close() and all children have completed Close() (but immediately before Done() is released)
+	TaskRef        any                     // Offered to you for open-ended use.
+	Label          string                  // Label is a log label and debugging
+	OnStart        func(ctx Context) error // Blocking fn called in StartChild(). If err, ctx.Close() is called and Go() returns the err and OnRun is never called.
+	OnRun          func(ctx Context)       // Async work body. If non-nil, ctx.Close() will be automatically called after OnRun() completes
+	OnClosing      func()                  // Called immediately after Close() is first called while self & children are still closing
+	OnChildClosing func(child Context)     // Called immediately after the child's OnClosing() is called
+	OnClosed       func()                  // Called after Close() and all children have completed Close() (but immediately before Done() is released)
 }
 
 type Context interface {
@@ -72,17 +73,23 @@ type Context interface {
 
 	// Async call that initiates task shutdown and causes all children's Close() to be called.
 	// Close can be called multiple times but calls after the first are in effect ignored.
-	// First, child processes get Close() in breath-first order.
+	// First, children get Close() in breath-first order.
 	// After all children are done closing, OnClosing(), then OnClosed() are executed.
 	Close() error
 
 	// Inserts a pending Close() on this Context once it is idle after the given delay.
 	// Subsequent calls will update the delay but the previously pending delay must run out first.
-	// If at the end of the period Task.OnRun() is complete and there are no children, then Close() is called.
+	// If at the end of the period Task.OnRun() is complete, there are no children, PreventIdleClose() is not in effect, then Close() is called.
 	CloseWhenIdle(delay time.Duration)
 
+	// Ensures that that this Context will not automatically idle-close until the given delay has passed.
+	// If previous PreventIdleClose calls were made, the more limiting delay is retained.
+	//
+	// Returns false if this Context has already been closed.
+	PreventIdleClose(delay time.Duration) bool
+
 	// Signals when Close() has been called.
-	// First, Child processes get Close(),  then OnClosing, then OnClosed are executing
+	// First, Children get Close(),  then OnClosing, then OnClosed are executing
 	Closing() <-chan struct{}
 
 	// Signals when Close() has fully executed, no children remain, and OnClosed() has been completed.
